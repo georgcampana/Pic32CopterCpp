5ca13594def27831011606469836aaa5b1908c7c
Georg Campana <georg.campana@gmail.com>
2013-04-01 17:23:16 +0200

    Devbits moved to system multiplier is now 20x PBCLK is now calculated correctly (not anymore /2 hardcoded)

--- system.cpp
+++ system.cpp
@@ -1,21 +1,43 @@
 /*
  * File:   system.cpp
  *
- * Work based on Jeff Rowberg's <jeff@rowberg.net> i2cdevlib
- * see at https://github.com/jrowberg/i2cdevlib
- *
- * Credits also to Noah Zerkin for the reverse enginering work
- * see at https://github.com/bzerk/MPU6050_DMP_6_axis_demo_
- *
- * Attempt is to adapt the code to the Microchip PIC32 platform
- * using an interrupt based i2c base class
- * and being more c++ish using the first Microchip C++ compiler
- * Author: Georg Campana
  *
  * Created on 22 ottobre 2012, 0.29
  */
 
 #include "include/system.h"
+
+
+// PIC32MX440F256H Configuration Bit Settings
+// DEVCFG3
+// USERID = No Setting
+
+// DEVCFG2
+#pragma config FPLLIDIV = DIV_2         // PLL Input Divider (2x Divider)
+#pragma config FPLLMUL = MUL_20         // PLL Multiplier (20x Multiplier)
+#pragma config UPLLIDIV = DIV_2         // USB PLL Input Divider (2x Divider)
+#pragma config UPLLEN = ON              // USB PLL Enable (Enabled)
+#pragma config FPLLODIV = DIV_1         // System PLL Output Clock Divider (PLL Divide by 1)
+
+// DEVCFG1
+#pragma config FNOSC = PRIPLL           // Oscillator Selection Bits (Primary Osc w/PLL (XT+,HS+,EC+PLL))
+#pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disabled)
+#pragma config IESO = ON                // Internal/External Switch Over (Enabled)
+#pragma config POSCMOD = HS             // Primary Oscillator Configuration (HS osc mode)
+#pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
+#pragma config FPBDIV = DIV_1           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/1)
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disable, FSCM Disabled)
+#pragma config WDTPS = PS1              // Watchdog Timer Postscaler (1:1)
+#pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled (SWDTEN Bit Controls))
+
+// DEVCFG0
+#pragma config DEBUG = OFF              // Background Debugger Enable (Debugger is disabled)
+#pragma config ICESEL = ICS_PGx2        // ICE/ICD Comm Channel Select (ICE EMUC2/EMUD2 pins shared with PGC2/PGD2)
+#pragma config PWP = OFF                // Program Flash Write Protect (Disable)
+#pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
+#pragma config CP = OFF                 // Code Protect (Protection Disabled)
+
+
 
 
  void System::DelayMS(UINT16 ms2delay) {
