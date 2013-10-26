d88965da3ee07a59cd5b9d7a5350535d520e122c
Georg Campana <georg.campana@gmail.com>
2013-10-26 22:09:37 +0200

    First MPU-9150 driver files: to be completed.

---
+++ srcs/driver/mpu-9150-regs.h
@@ -0,0 +1,66 @@
+/* 
+ * File:   mpu-9150-regs.h
+ * Author: georg
+ *
+ * Created on 24 ottobre 2013, 11.51
+ */
+
+#ifndef MPU_9150_REGS_H
+#define	MPU_9150_REGS_H
+
+
+enum MPU9150_REGS {
+    REG_YG_OFFS_TC  = 0x01,
+    REG_ACCELL_OFFSETS = 0x06
+
+};
+
+
+#if defined MPU6050
+const struct gyro_reg_s reg = {
+    .who_am_i       = 0x75,
+    .rate_div       = 0x19,
+    .lpf            = 0x1A,
+    .prod_id        = 0x0C,
+    .user_ctrl      = 0x6A,
+    .fifo_en        = 0x23,
+    .gyro_cfg       = 0x1B,
+    .accel_cfg      = 0x1C,
+    .motion_thr     = 0x1F,
+    .motion_dur     = 0x20,
+    .fifo_count_h   = 0x72,
+    .fifo_r_w       = 0x74,
+    .raw_gyro       = 0x43,
+    .raw_accel      = 0x3B,
+    .temp           = 0x41,
+    .int_enable     = 0x38,
+    .dmp_int_status = 0x39,
+    .int_status     = 0x3A,
+    .pwr_mgmt_1     = 0x6B,
+    .pwr_mgmt_2     = 0x6C,
+    .int_pin_cfg    = 0x37,
+    .mem_r_w        = 0x6F,
+    .accel_offs     = 0x06,
+    .i2c_mst        = 0x24,
+    .bank_sel       = 0x6D,
+    .mem_start_addr = 0x6E,
+    .prgm_start_h   = 0x70
+#ifdef AK89xx_SECONDARY
+    ,.raw_compass   = 0x49,
+
+    .s0_addr        = 0x25,
+    .s0_reg         = 0x26,
+    .s0_ctrl        = 0x27,
+    .s1_addr        = 0x28,
+    .s1_reg         = 0x29,
+    .s1_ctrl        = 0x2A,
+    .s4_ctrl        = 0x34,
+    .s0_do          = 0x63,
+    .s1_do          = 0x64,
+    .i2c_delay_ctrl = 0x67
+#endif
+};
+
+
+#endif	/* MPU_9150_REGS_H */
+
---
+++ srcs/driver/mpu-9150.h
@@ -0,0 +1,57 @@
+/* 
+ * File:   mpu-9150.h
+ * Author: georg
+ *
+ * Created on 24 ottobre 2013, 11.39
+ */
+
+#ifndef MPU_9150_H
+#define	MPU_9150_H
+
+#include "i2cmanager.h"
+
+enum MPU9150_I2C_ADDR  {
+    MPU9150_ADDRESS_AD0_LOW = 0x68,
+    MPU9150_ADDRESS_AD0_HIGH = 0x69,
+    MPU9150_DEFAULT_ADDRESS = MPU9150_ADDRESS_AD0_LOW
+};
+
+
+
+
+class MPU_9150 : public SingleAccessDevice {
+
+    UINT8 i2caddr;
+    I2c& i2cmanager;
+
+    const static UINT8 dmpMemory[];
+    const static UINT8 dmpConfig[];
+    const static UINT8 dmpUpdates[];
+
+    UINT8 tempworkbuffer[128];
+
+    // State machine for interrupt trasfers using the i2c
+    enum I2cStateMachine {
+        I2C_NONE = 0,
+        I2C_CTRL_READ,
+        I2C_CTRL_WRITTEN
+    } i2cstate;
+    enum I2cTransType {
+        I2C_TRANS_NONE = 0,
+        I2C_DMP_ON,
+        I2C_DMP_OFF
+    } i2ctrans;
+    // used across i2c transfers
+    UINT8 tmpregvalue;
+  public:
+
+    MPU_9150(I2c& busmanager, UINT8 busaddress = MPU9150_DEFAULT_ADDRESS );
+    bool Init();
+
+};
+
+
+
+
+#endif	/* MPU_9150_H */
+
