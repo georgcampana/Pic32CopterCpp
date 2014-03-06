155603075d32ed2ba70fdf5f6d800e29cf409dea
Georg Campana <georg.campana@gmail.com>
2014-03-06 17:05:42 +0100

    fixed and tested SPIManager (seems to work with WifiModule)

--- srcs/haldriver/spimanager.cpp
+++ srcs/haldriver/spimanager.cpp
@@ -36,15 +36,24 @@ extern "C" {
 #endif
 
 void
-//__ISR(_UART_1_VECTOR, ipl5)
+//__ISR(_SPI_2_VECTOR, ipl5)
 Spi1IntService(void){
     spi1_ref->handleInterrupt();
 }
 
 void
-//__ISR(_UART_2_VECTOR, ipl5)
 Spi2IntService(void){
     spi2_ref->handleInterrupt();
+}
+
+void // DMA_CHANNEL2
+Dma3IntService(void) {
+    spi2_ref->dmaHandleInterruptTx();
+}
+
+void // DMA_CHANNEL3
+Dma4IntService(void) {
+    spi2_ref->dmaHandleInterruptRx();
 }
 
 #ifdef	__cplusplus
@@ -53,19 +62,119 @@ Spi2IntService(void){
 
 
 
-SpiManager::SpiManager(SpiChannel chan, UInt32 baud = 115200) : channel(chan) {
+SpiManager::SpiManager(SpiChannel chan, UInt32 baud, OutputPin* cs) : channel(chan),
+                         dmachanneltx(DMA_CHANNEL2), dmachannelrx(DMA_CHANNEL3), chipselect(cs) {
 
-    ::SpiChnOpen(channel, SPI_OPEN_MSTEN | SPI_OPEN_MODE8 | SPI_OPEN_TBE_HALF_EMPTY | SPI_OPEN_RBF_NOT_EMPTY);
+    if(chipselect)chipselect->set(true); // deactivate chip interface;
+
+    ::SpiChnEnable(channel, 0);
+    ::SpiChnOpen(channel, (SpiOpenFlags) (SPI_OPEN_MSTEN | SPI_OPEN_MODE8) /* | SPI_OPEN_TBE_HALF_EMPTY | SPI_OPEN_RBF_NOT_EMPTY */,4);
+    ::SpiChnSetBitRate(channel, GetPeripheralClock(), baud);
     ::SpiChnEnable(channel, true);
+
+#ifdef _SPI_1_VECTOR  
+    spi1_ref = this;
+#endif
+#ifdef _SPI_2_VECTOR  
+    spi2_ref = this;
+
+    //DMA SPI TX (pri same as the RX to be sure that it will perform roundrobin)
+    DmaChnOpen(dmachanneltx, DMA_CHN_PRI3, DMA_OPEN_DEFAULT);
+    DmaChnSetEventControl(dmachanneltx, (DmaEvCtrlFlags)( DMA_EV_START_IRQ_EN | DMA_EV_START_IRQ(_SPI2_TX_IRQ)));
+
+    //DmaChnSetEvEnableFlags(dmachanneltx, DMA_EV_BLOCK_DONE);
+
+    INTSetVectorPriority((INT_VECTOR)INT_VECTOR_DMA(dmachanneltx), INT_PRIORITY_LEVEL_5);
+    INTSetVectorSubPriority((INT_VECTOR)INT_VECTOR_DMA(dmachanneltx), INT_SUB_PRIORITY_LEVEL_3);
+
+    //INTEnable((INT_SOURCE)INT_SOURCE_DMA(dmachanneltx), INT_ENABLED);
+
+    //DMA SPI RX
+    DmaChnOpen(dmachannelrx, DMA_CHN_PRI3,DMA_OPEN_DEFAULT);
+    DmaChnSetEventControl(dmachannelrx, (DmaEvCtrlFlags)(DMA_EV_START_IRQ_EN| DMA_EV_START_IRQ(_SPI2_RX_IRQ)));
+
+    DmaChnSetEvEnableFlags(dmachannelrx, DMA_EV_BLOCK_DONE);
+    
+    INTSetVectorPriority((INT_VECTOR)INT_VECTOR_DMA(dmachannelrx), INT_PRIORITY_LEVEL_5);
+    INTSetVectorSubPriority((INT_VECTOR)INT_VECTOR_DMA(dmachannelrx), INT_SUB_PRIORITY_LEVEL_3);
+
+    INTEnable((INT_SOURCE)INT_SOURCE_DMA(dmachannelrx), INT_ENABLED);
+
+#endif
+
 
 }
 
-bool SpiManager::transaction(UInt8* txdata, UInt8* rxdata, UInt32 datalen) {
+
+bool SpiManager::transaction(UInt8* txdata, UInt8* rxdata, UInt32 len) {
+
+    UInt32 dummy = *((UInt32*)SpiChnBuffer(channel)); // this is to free the read buffer
+    ::SpiChnGetRov(channel, true); // this is to reset any overflow condition
+
+    waitingtask = Kernel::GetRunningTask();
+
+    if(chipselect) chipselect->set(false) ;
+
+    ::SpiChnGetRov(channel, true);
+
+    ::DmaChnSetTxfer(dmachannelrx, (void*)SpiChnBuffer(channel), rxdata, 1, len, 1 );
+    ::DmaChnEnable(dmachannelrx);
+
+    ::DmaChnSetTxfer(dmachanneltx, txdata, (void*)SpiChnBuffer(channel), len, 1, 1 );
+    ::DmaChnStartTxfer(dmachanneltx, DMA_WAIT_NOT, 0 );
 
 
+    TaskDefaultWait(waitingtask, blockingtimeout);
+    waitingtask = NULL; // let's avoid spurious signals
 
+    if(chipselect) chipselect->set(true) ;
 }
 
 void SpiManager::handleInterrupt() {
 
+}
+
+void SpiManager::setChipSelectPin(OutputPin* cs) {
+    chipselect = cs;
+    if(chipselect)chipselect->set(true); // deactivate chip interface;
+}
+
+void SpiManager::dmaHandleInterruptRx() {
+
+    //event flags causing the interruptt
+    int eventflags;
+
+    //get the event flags generating the interrupt
+    eventflags=DmaChnGetEvFlags(dmachannelrx);
+    //clear for further ints
+    INTClearFlag((INT_SOURCE)INT_SOURCE_DMA(dmachannelrx));
+
+    if(eventflags & DMA_EV_BLOCK_DONE)
+    {
+        DmaChnClrEvFlags(dmachannelrx, DMA_EV_BLOCK_DONE);
+        SignalWaitingTask(waitingtask);
+    }
+    else {
+        // should never happen because only block complete is enabled (unless an error happened)
+        DmaChnClrEvFlags(dmachannelrx, (DmaEvFlags)eventflags);
+    }
+}
+
+void SpiManager::dmaHandleInterruptTx() {
+    //event flags causing the interruptt
+    int eventflags;
+
+    //get the event flags generating the interrupt
+    eventflags=DmaChnGetEvFlags(dmachanneltx);
+    //clear for further ints
+    INTClearFlag((INT_SOURCE)INT_SOURCE_DMA(dmachanneltx));
+
+    if(eventflags & DMA_EV_BLOCK_DONE)
+    {
+        DmaChnClrEvFlags(dmachanneltx, DMA_EV_BLOCK_DONE);
+    }
+    else {
+        // should never happen because only block complete is enabled (unless an error happened)
+        DmaChnClrEvFlags(dmachanneltx, (DmaEvFlags)eventflags);
+    }
 }
--- srcs/haldriver/spimanager.h
+++ srcs/haldriver/spimanager.h
@@ -20,20 +20,27 @@
 
 #include "../kernel/hal/hal.h"
 #include "../kernel/device.h"
+#include "digitaliomanager.h"
 
 
 class SpiManager : public SingleAccessDevice {
 
     SpiChannel channel;
+    OutputPin* chipselect;
 
+    DmaChannel dmachanneltx; // DMA enabled; -1 = no DMA usage
+    DmaChannel dmachannelrx; // DMA enabled; -1 = no DMA usage
 
 public:
-    SpiManager(SpiChannel chan, UInt32 baud = 115200);
+    SpiManager(SpiChannel chan, UInt32 baud = 1000000, OutputPin* cs=NULL);
 
-    bool transaction(UInt8* txdata, UInt8* rxdata, UInt32 datalen);
+    void setChipSelectPin(OutputPin* cs);
+
+    bool transaction(UInt8* txdata, UInt8* rxdata, UInt32 len);
 
     void handleInterrupt();
-
+    void dmaHandleInterruptRx();
+    void dmaHandleInterruptTx();
 };
 
 
