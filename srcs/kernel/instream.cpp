3b8ccabb01c3de2ba82fc683323536f6cf494baa
Georg Campana <georg.campana@gmail.com>
2014-03-06 17:04:53 +0100

    new instream class to stream and parse directly from a stream source (TO TEST)

---
+++ srcs/kernel/instream.cpp
@@ -0,0 +1,167 @@
+/*
+ * Pic32CopterCpp -- A C++ microOS for the PIC32
+ *
+ * Copyright (C) 2012 - 2014, Georg Campana
+ *
+ * Georg Campana <g.campana(AT)stetel.com>
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE.txt file
+ * at the top of the source tree.
+ *
+ * File:   instream.cpp
+ * Author: Georg Campana
+ *
+ * Created on 5 marzo 2014, 11.47
+ */
+
+#include "instream.h"
+
+
+const Char InStream::defaultseparators[] = ", \n";
+
+Int64 InStream::Ascii2Integer(UInt64 maxpositive, UInt64 minabsnegative) {
+    bool isnegative;
+    Int64 number = 0;
+
+    Int16 currentchar = indevice.getChar(); // should wait (or timeout)
+    if((Char)currentchar == '+') { currentchar = indevice.getChar(); }
+    else if((Char)currentchar == '-') { currentchar = indevice.getChar(); isnegative == true;}
+
+    while (currentchar != -1 && (Char)currentchar >= '0' && (Char)currentchar <='9') {
+        number = (number<<3)+(number<<1)+(currentchar)-'0';
+        currentchar = indevice.getChar();
+    }
+
+    // no more numbers.. we check if we have a separator now
+    bool separatorfound = false;
+    
+    if((Char)currentchar == '\r') { currentchar = indevice.getChar(); }
+    if(currentchar!=-1) {
+        for(Char const* currseparator = fieldseparators; *currseparator != NULL; currseparator++) {
+            if((Char)currentchar == *currseparator) {
+                separatorfound=true;
+                break;
+            }
+        }
+        if(separatorfound==false) {
+            lasterror = IS_NotANumber;
+            // let's seek for the next separator
+            while(separatorfound==false && currentchar!=-1) {
+                currentchar = indevice.getChar();
+                for(Char const* currseparator = fieldseparators; *currseparator != NULL; currseparator++) {
+                    if((Char)currentchar == *currseparator) {
+                        separatorfound=true;
+                        break;
+                    }
+                }
+
+            }
+        }
+    }
+    
+    if(currentchar==-1) {
+        // timeout error
+        lasterror = IS_Timeout;
+    }
+
+
+
+    if(isnegative) {
+        if(number > minabsnegative) lasterror = IS_DecimatedToFit;
+        number = -number;
+    }
+    else {
+        if(number > maxpositive) lasterror = IS_DecimatedToFit;
+    }
+
+    return number;
+}
+
+
+void InStream::setSeparators(const Char* newseparators) {
+    fieldseparators = (newseparators)?  newseparators : defaultseparators;
+}
+
+void InStream::setMaxStringLen(Int32 maxchars) {
+    maxstrlen = maxchars;
+}
+
+InStream::InStream(CharStreamDevice& serialif, Int32 initialmaxlen) :
+                          indevice(serialif), fieldseparators(defaultseparators),
+                          maxstrlen(0), lasterror(IS_OK) {
+
+}
+//    InStream::~InStream();
+
+InStream& InStream::operator >> (Int32& number) {
+
+    Int64 readnumber = Ascii2Integer(0x7fffffff, 0x80000000);
+    number = (Int32)readnumber; // possible loss of information (marked as error by Ascii2Integer)
+
+    return *this;
+}
+
+InStream& InStream::operator >> (UInt32& unumber) {
+    Int64 readnumber = Ascii2Integer(0x7fffffff, 0x7fffffff);
+    if(readnumber < 0) {
+        readnumber=-readnumber;
+        if(lasterror == IS_OK) lasterror = IS_NegativeInUnsigned;
+    }
+    unumber = (UInt32)readnumber; // possible loss of information (marked as error by Ascii2Integer)
+
+    return *this;
+}
+
+InStream& InStream::operator >> (Char* string) {
+
+    bool separatorfound = false;
+    Int32 maxchars = maxstrlen;
+
+    while(separatorfound==false && maxchars > 0) {
+        Int16 currentchar = indevice.getChar(); // should wait (or timeout)
+        if(currentchar == -1) {
+            lasterror = IS_Timeout;
+            break;
+        }
+        for(Char const* currseparator = fieldseparators; *currseparator != NULL; currseparator++) {
+            if((Char)currentchar == *currseparator) {
+                separatorfound=true;
+                break;
+            }
+        }
+        if(separatorfound==false) {
+            // ok it's a char that is not a separator or a timeout: We copy it
+            *string++ = (Char)currentchar;
+            maxchars--;
+        }
+    }
+
+    if(maxchars == 0 && separatorfound == false) {
+        lasterror = IS_StringOverflow;
+    }
+
+    return *this;
+}
+
+InStream& InStream::operator >> (Char& singlechar) {
+    Int16 currentchar = indevice.getChar(); // should wait (or timeout)
+    if(currentchar != -1) {
+        lasterror = IS_Timeout;
+    }
+    else {
+        singlechar = (Char)currentchar;
+    }
+
+    return *this;
+}
+
+InStream& InStream::operator >> (bool& onezero) {
+
+    return *this;
+}
+
+InStream& InStream::operator >> (float& fnumber) {
+
+    return *this;
+}
---
+++ srcs/kernel/instream.h
@@ -0,0 +1,66 @@
+/*
+ * Pic32CopterCpp -- A C++ microOS for the PIC32
+ *
+ * Copyright (C) 2012 - 2014, Georg Campana
+ *
+ * Georg Campana <g.campana(AT)stetel.com>
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License Version 2. See the LICENSE.txt file
+ * at the top of the source tree.
+ *
+ * File:   instream.h
+ * Author: Georg Campana
+ *
+ * Created on 5 marzo 2014, 11.47
+ */
+
+#ifndef INSTREAM_H
+#define	INSTREAM_H
+
+#include "device.h"
+
+class InStream {
+    CharStreamDevice& indevice;
+
+    Char const* fieldseparators;
+    static const Char defaultseparators[];
+    Int32 maxstrlen;
+
+    Int64 Ascii2Integer(UInt64 maxpositive, UInt64 minabsnegative);
+public:
+
+    enum IS_Error {
+        IS_OK = 0,
+        IS_NotANumber,
+        IS_StringOverflow,
+        IS_DecimatedToFit,
+        IS_Timeout, // no more data received
+        IS_NegativeInUnsigned
+    } lasterror;
+
+    void resetError();
+    void setSeparators(const Char* newseparators);
+    void setMaxStringLen(Int32 maxchars);
+
+    InStream(CharStreamDevice& serialif, Int32 initialmaxlen=0);
+//    ~InStream();
+
+    InStream& operator >> (Int32& number) ;
+    InStream& operator >> (UInt32& unumber) ;
+    InStream& operator >> (Int8& cnumber) ;
+    InStream& operator >> (UInt8& ucnumber) ;
+    InStream& operator >> (Char* string) ;
+    InStream& operator >> (Char& singlechar) ;
+    InStream& operator >> (bool& onezero) ;
+    InStream& operator >> (float& fnumber) ;
+};
+
+inline  InStream& InStream::operator >> (Int8& cnumber) { return this->operator >>((Int32&)cnumber);};
+inline  InStream& InStream::operator >> (UInt8& ucnumber) { return this->operator >>((UInt32&)ucnumber);};
+inline  void InStream::resetError() { lasterror = IS_OK; }
+
+
+
+#endif	/* INSTREAM_H */
+
